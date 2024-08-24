682386a10c5c4d3b8dd20a129b3abf1a2a57f973
akafuda <aoto0630@keio.jp>
2024-08-24 17:09:04 +0900

    [ADDED] /groupselect page.tsx

---
+++ apps/web-app/src/app/(akafuda)/groupselect/layout.tsx
@@ -0,0 +1,6 @@
+// app/(custom-layout)/dao-directory/layout.tsx
+import Providers from "../../providers"
+
+export default function CustomLayout({ children }: { children: React.ReactNode }) {
+    return <Providers>{children}</Providers>
+}
